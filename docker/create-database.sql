44784e1942e01c23729a2346fe9e4e52db3bb241
zlbovolini <lucas.bovolini@zup.com.br>
2021-11-04 11:01:42 -0300

    Modificado precisao do tempo para microsegundos

--- docker/create-database.sql
+++ docker/create-database.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `estabelecimento`.`estabelecimento` (
   `uuid` BINARY(16) NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
   `cnpj` VARCHAR(45) NOT NULL,
-  `criado_em` DATETIME NOT NULL,
+  `criado_em` DATETIME(6) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE,
