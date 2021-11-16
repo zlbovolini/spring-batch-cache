cff595de22a9b8a7c581c746b64956c41ed8fbf4
zlbovolini <lucas.bovolini@zup.com.br>
2021-11-16 14:51:43 -0300

    Adicionado representacao de base de dados legado

---
+++ docker/legado/create-legacy-database.sql
@@ -0,0 +1,60 @@
+BEGIN;
+
+CREATE SCHEMA estabelecimentos;
+
+CREATE TABLE IF NOT EXISTS estabelecimentos.tb_estabelecimento(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    nome text,
+    cnpj bigint NOT NULL,
+    cliente text,
+    PRIMARY KEY (id)
+);
+
+-- polling
+CREATE TABLE IF NOT EXISTS estabelecimentos.novo_estabelecimento(
+    id      integer NOT NULL,
+    nome    text,
+    cnpj    bigint,
+    cliente text
+);
+
+CREATE TYPE estabelecimentos.estabelecimento_type AS (
+    id      integer,
+    nome    text,
+    cnpj    bigint,
+    cliente text
+);
+
+
+CREATE OR REPLACE PROCEDURE estabelecimentos.busca_estabelecimentos(OUT estabelecimento estabelecimentos.estabelecimento_type)
+AS $$
+BEGIN
+    SELECT id, nome, cnpj, cliente
+    FROM   estabelecimentos.tb_estabelecimento
+    LIMIT  2
+	INTO   estabelecimento;
+END $$
+LANGUAGE PLPGSQL;
+
+
+-- insere na tabela de polling
+CREATE OR REPLACE FUNCTION estabelecimentos.novo_estabelecimento_criado()
+RETURNS TRIGGER
+AS $$
+BEGIN
+   INSERT INTO estabelecimentos.novo_estabelecimento (id, nome, cnpj, cliente)
+   VALUES (NEW.id, UPPER(NEW.nome), NEW.cnpj, UPPER(NEW.cliente));
+
+   RETURN NEW;
+END $$
+LANGUAGE PLPGSQL;
+
+
+-- a cada estabelecimento criado
+CREATE OR REPLACE TRIGGER notifica_novo_estabelecimento_criado
+   AFTER INSERT
+   ON estabelecimentos.tb_estabelecimento
+   FOR EACH ROW
+       EXECUTE PROCEDURE estabelecimentos.novo_estabelecimento_criado();
+
+END;
