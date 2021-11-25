d5b261111330771290fa03579d95158607240012
zlbovolini <lucas.bovolini@zup.com.br>
2021-11-25 15:13:09 -0300

    Atualizado Spring Boot e adicionado dependecia postgres

--- src/main/java/com/github/lbovolini/app/estabelecimento/configuration/SwaggerConfiguration.java
+++ src/main/java/com/github/lbovolini/app/estabelecimento/configuration/SwaggerConfiguration.java
@@ -2,31 +2,25 @@ package com.github.lbovolini.app.estabelecimento.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-@Configuration
+//@Configuration
 class SwaggerConfiguration {
 
-    @Bean
-    Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.github.lbovolini.app"))
-                .paths(PathSelectors.ant("/**"))
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Estabelecimentos API")
-                .description("API CRUD de estabelecimentos")
-                .version("0.0.1")
-                .build();
-    }
+    //@Bean
+//    Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.github.lbovolini.app"))
+//                .paths(PathSelectors.ant("/**"))
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Estabelecimentos API")
+//                .description("API CRUD de estabelecimentos")
+//                .version("0.0.1")
+//                .build();
+//    }
 }
